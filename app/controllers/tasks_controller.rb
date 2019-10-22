c2de0163930ad614cb4c97017a1ecfd3da29a64d
fumischo <nil.admirari1009@gmail.com>
2019-10-22 19:55:13 +0900

    タスクの一覧、作成, 詳細、編集、削除画面作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,58 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update,:destroy]
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if params[:back]
+      render :new
+    else
+      if @task.save
+        redirect_to root_path, notice: "登録完了"
+      else
+        render :new
+      end
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update!(task_params)
+    redirect_to tasks_path, notice: "更新完了"
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, notice: "削除完了"
+  end
+
+  def confirm
+    @task= Task.new(task_params)
+    render :new if @task.invalid?
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
