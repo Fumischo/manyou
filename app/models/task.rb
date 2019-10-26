ae67aa1e1ba4c3894b2f8f983948e76eff915874
fumischo <nil.admirari1009@gmail.com>
2019-10-26 16:57:32 +0900

    step2実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
+  belongs_to :user
 end
-
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Manyou
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja # デフォルトのlocaleを日本語(:ja)にする
 
     # Settings in config/environments/* take precedence over those specified here.
     config.generators do |g|
@@ -24,3 +27,4 @@ module Manyou
     end 
   end
 end
+
