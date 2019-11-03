3e6e108e6ee359370d2d268e0011457e4847cd95
fumischo <nil.admirari1009@gmail.com>
2019-11-03 11:27:56 +0900

    newアクション、showアクション、usersルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :users
-  end
+  root to: 'tasks#index'
+  resources :users
   root to: 'tasks#index'
   resources :tasks do
     collection do
