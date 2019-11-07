17a55db902d65f507e2ff1d3949e73da8e7f660c
fumischo <nil.admirari1009@gmail.com>
2019-11-07 16:31:19 +0900

    Top pageを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-  root to: 'tasks#index'
+  root to: 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   resources :tasks 
